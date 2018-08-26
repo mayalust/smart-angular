6ab64338e22b8f35892ebea852f313c6ad3fdd1b
Lin Jingbin <linjingbin@proudsmart.com>
2018-08-26 12:23:07 +0800

    v1.0.43

--- dist/run.js
+++ dist/run.js
@@ -1,6 +1,12 @@
 #!/usr/bin/env node
 const fn = require("./index");
+function getString(str){
+  let stringExp = /\"?(\w+)\"?/g,
+    match = stringExp.exec(str);
+  return match ? match[1] : null;
+}
 let arguments = process.argv.slice(2),
-  command = arguments[0] || "pack",
-  name = arguments[1] || "core";
+  command = getString(arguments[0]) || "pack",
+  name = getString(arguments[1]) ||  "core";
+  console.log(command, name);
 fn(command, name);
